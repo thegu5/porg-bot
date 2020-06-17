0607a3d70c698841eee5c4f83b1610df448a4d27
thegu5 <thegu5@github.com>
2020-06-17 19:41:44 -0400

    porg-song now has variation

--- commands/misc/porg-song.js
+++ commands/misc/porg-song.js
@@ -16,19 +16,44 @@ module.exports = class PorgSong extends Command {
 
 	async run(message) {
 		if (message.member.voice.channel) {
-			const connection = await message.member.voice.channel.join();
-			const dispatcher = connection.play('porg-wars-theme.mp3');
-			dispatcher.on('start', () => {
-				message.say('porg-wars-theme.mp3 is now playing!\nSong Created by Volpe.');
-			});
+			const songnum = Math.floor(Math.random() * 3) + 1
+			if (songnum === 1) {
+				const connection = await message.member.voice.channel.join();
+				const dispatcher = connection.play('porg-wars-theme.mp3');
+				dispatcher.on('start', () => {
+					message.say('porg-wars-theme.mp3 is now playing!\nSong Created by Volpe.');
+				});
 
-			dispatcher.on('finish', () => {
-				message.say('porg-wars-theme.mp3 has finished playing!');
-				connection.disconnect();
-			});
+				dispatcher.on('finish', () => {
+					message.say('porg-wars-theme.mp3 has finished playing!');
+					connection.disconnect();
+				});
+				dispatcher.on('error', console.error);
+			} else if (songnum === 2) {
+				const connection = await message.member.voice.channel.join();
+				const dispatcher = connection.play('imperial-porg.mp3');
+				dispatcher.on('start', () => {
+					message.say('imperial-porg.mp3 is now playing!\nSong Created by Lewis Walker.');
+				});
 
-			// Always remember to handle errors appropriately!
-			dispatcher.on('error', console.error);
+				dispatcher.on('finish', () => {
+					message.say('imperial-porg.mp3 has finished playing!');
+					connection.disconnect();
+				});
+				dispatcher.on('error', console.error);
+			} else if (songnum === 3) {
+				const connection = await message.member.voice.channel.join();
+				const dispatcher = connection.play('duel-of-the-porgs.mp3');
+				dispatcher.on('start', () => {
+					message.say('duel-of-the-porgs.mp3 is now playing!\nSong Created by Volpe.');
+				});
+
+				dispatcher.on('finish', () => {
+					message.say('duel-of-the-porgs.mp3 has finished playing!');
+					connection.disconnect();
+				});
+				dispatcher.on('error', console.error);
+			}
 		} else {
 			return message.say('Join a voice channel, you dum dum')
 		}
