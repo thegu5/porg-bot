e70e6d044c9c4b2fc4c40fb785a643bba0baf710
thegu5 <thegu5@github.com>
2020-06-18 20:40:10 -0400

    make the server command cool

--- commands/data/server.js
+++ commands/data/server.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
 module.exports = class ServerCommand extends Command {
 	constructor(client) {
 		super(client, {
@@ -15,6 +16,30 @@ module.exports = class ServerCommand extends Command {
 	}
 
 	run(message) {
-        message.say(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+		const embed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Server Info')
+			.setThumbnail(message.guild.iconURL({ format: 'png' }))
+			.addFields(
+				{ name: 'Created at:', value: message.guild.createdTimestamp, inline: true },
+				{ name: 'Members:', value: message.guild.memberCount, inline: true },
+			)
+			.addFields(
+				{ name: 'Guild id:', value: message.guild.id, inline: true },
+				{ name: 'Shard id', value: message.guild.shardID, inline: true },
+			)
+			.addFields(
+				{ name: 'Partnered?', value: message.guild.partnered, inline: true },
+				{ name: 'Verified?', value: message.guild.verified, inline: true },
+			)
+			.addFields(
+				{ name: 'Verification Level:', value: message.guild.verificationLevel, inline: true },
+				{ name: 'Nitro boost level:', value: message.guild.premiumTier, inline: true },
+			)
+			.setTimestamp()
+		if (message.guild.description) {
+			embed.addFields({ name: 'Description', value: message.guild.description })
+		}
+		message.channel.send(embed);
 	}
 };
