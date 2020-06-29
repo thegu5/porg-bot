0f317b05544c9b87d339d161e543fe3c656c108a
thegu5 <thegu5@github.com>
2020-06-29 13:31:16 -0400

    p!meme is an embed

--- commands/data/meme.js
+++ commands/data/meme.js
@@ -1,5 +1,6 @@
 const { Command } = require('discord.js-commando');
 const fetch = require('node-fetch')
+const Discord = require('discord.js');
 module.exports = class MemeCommand extends Command {
 	constructor(client) {
 		super(client, {
@@ -17,7 +18,18 @@ module.exports = class MemeCommand extends Command {
 	 run(message) {
         fetch('https://meme-api.herokuapp.com/gimme')
             .then(res => res.json())
-            .then(json => message.say(json.url));
-
+			.then(json => {
+				const color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+				const embed = new Discord.MessageEmbed()
+					.setTitle('r/'+json.subreddit + ' - ' + json.title)
+					.setColor(color)
+					.setImage(json.url)
+				if (json.title === 'Me_irl' || json.title === 'me_irl' || json.title === 'Me_Irl') {
+					embed.setTitle('r/'+json.subreddit)
+				}
+				message.say(embed)
+			});
+			
+		
     }
 };
